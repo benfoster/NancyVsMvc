97199978b4dfe01155dc4c2042337dd684585b66
Ben Foster <ben@planetcloud.co.uk>
2013-04-01 16:34:11 +0100

    Getting Started - Mvc

---
+++ src/NancyVsMvc.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NancyVsMvc.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeMessage = "Welcome to ASP.NET MVC!";
+            return View();
+        }
+
+    }
+}
