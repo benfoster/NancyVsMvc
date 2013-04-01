9d29254652f3485cd2a90b8a94d106cb7385e7b7
Ben Foster <ben@planetcloud.co.uk>
2013-04-01 16:49:16 +0100

    Getting Started - Nancy

---
+++ src/NancyVsMvc.Nancy/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NancyVsMvc.Nancy.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {
+                return View["home", new { WelcomeMessage = "Welcome to Nancy!" }];
+            };
+        }
+    }
+}
