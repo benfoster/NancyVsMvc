bbe203f105d475c11bb79cf93cfaf2b70cf8686b
Ben Foster <ben@planetcloud.co.uk>
2013-04-03 23:33:37 +0100

    Static Content - Nancy

---
+++ src/NancyVsMvc.Nancy/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace NancyVsMvc.Nancy
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/assets")
+            );
+
+            nancyConventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/", "/_root", new[] { ".png", ".txt" })
+            );
+        }
+    }
+}
---
+++ src/NancyVsMvc.Nancy/Custom404Handler.cs
@@ -0,0 +1,37 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.ViewEngines;
+using System;
+using System.Linq;
+
+namespace NancyVsMvc.Nancy
+{
+    public class Custom404Handler : IStatusCodeHandler
+    {
+        private readonly IViewFactory viewFactory;
+
+        public Custom404Handler(IViewFactory viewFactory)
+        {
+            if (viewFactory == null)
+            {
+                throw new ArgumentNullException("viewFactory");
+            }
+
+            this.viewFactory = viewFactory;
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var viewRenderer = new DefaultViewRenderer(viewFactory);
+            var response = viewRenderer.RenderView(context, "404");
+            context.Response = response;
+            context.Response.StatusCode = HttpStatusCode.NotFound;
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound
+                && context.Request.Headers.Accept.Any(x => x.Item1 == "text/html");
+        }
+    }
+}
